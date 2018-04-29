d12a5ac1cdbafc365871b31785279e849a1fc652
py-john <pyjohn3@gmail.com>
2018-04-29 01:19:33 -0700

    specify module logger

--- newswire_check.py
+++ newswire_check.py
@@ -4,17 +4,20 @@ import json
 import logging
 
 from requests_html import HTML, HTMLSession
-from textmyself import textmyself
+from mysms import textmyself
 
 JSON_URL = 'http://www.rockstargames.com/newswire/get-posts.json?page=1'
 PACKAGE_DIR = os.path.dirname(os.path.realpath(__file__))
 LOG_FILE = f'{PACKAGE_DIR}/newswire_check.log'
 JSON_FILE = f'{PACKAGE_DIR}/newswire.json'
 
-logging.basicConfig(level=logging.INFO,
-                    filename=LOG_FILE,
+logging.basicConfig(filename=LOG_FILE,
                     format="%(asctime)s - %(message)s",
                     datefmt='%F %T')
+
+logger = logging.getLogger(__name__)
+logger.setLevel('INFO')
+
 
 def get_last_title(j):
     """Get the last post title, the json contains html tags"""
@@ -22,18 +25,21 @@ def get_last_title(j):
     last_title = HTML(html=last_post).text
     return last_title
 
-def new_title(title):
-    """Open and compare title to last saved title, returns boolean if updated"""
-    with open(JSON_FILE, 'r') as f:
-        stored = json.load(f)
 
+def new_title(title):
+    """Open and compare title to last saved title. Return boolean if updated"""
+    try:
+        with open(JSON_FILE, 'r') as f:
+            stored = json.load(f)
+    except FileNotFoundError:
+        stored = {'Last Title': ''}
     if title != stored['Last Title']:
         stored['Last Title'] = title
         with open(JSON_FILE, 'w') as f:
             json.dump(stored, f, indent=4)
         return True
-
     return False
+
 
 def main():
     """Run the scraper with the json response and text new title if updated """
@@ -42,9 +48,10 @@ def main():
     title = get_last_title(json_res)
     if new_title(title):
         textmyself(title)
-        logging.info('Updated: ' + title)
+        logger.info('Updated: ' + title)
     else:
-        logging.info('Checked')
+        logger.info('Checked')
+
 
 if __name__ == '__main__':
     main()
